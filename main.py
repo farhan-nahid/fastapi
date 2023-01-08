e343cadc4b5de00ae72d57b9cace3d3f21117d4b
Farhan Ahmed Nahid <nahidahmed9933@gmail.com>
2023-01-09 00:22:07 +0600

    blog added

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ def read_root():
 @app.get("/about")
 def about():
     return {"data":"about page"}
+
+
+@app.get("/blogs")
+def blog():
+    return {"data": ["blog1", "blog2", "blog3"]}
+
+
+@app.get("/blog/{id}")
+def blog(id):
+    return {"data":{"blog_id": id}}
+
