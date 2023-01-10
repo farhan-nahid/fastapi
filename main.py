40192dacf5ad1bfac02c95ded23ab3efb4e49bcb
Farhan Ahmed Nahid <nahidahmed9933@gmail.com>
2023-01-10 22:39:48 +0600

    delete blog added

--- main.py
+++ main.py
@@ -61,3 +61,14 @@ def get_blog_by_id(id:int, db: Session= Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
     else :
         return blog
+    
+
+@app.delete("/blog/{id}", status_code=status.HTTP_200_OK)
+def destroy_blog(id:int, db: Session= Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} is not available")
+    else :
+        blog.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "Blog deleted successfully"}
