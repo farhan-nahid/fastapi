09873bcfecf2cb818321aee15eab0bab792460f4
Farhan Ahmed Nahid <nahidahmed9933@gmail.com>
2023-01-10 22:03:53 +0600

    blog post & get added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
+from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 from database.database import *
-from fastapi import Depends, FastAPI
 from models import models
 from schemas.schemas import Blog
 
@@ -11,6 +11,13 @@ app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -22,11 +29,6 @@ def read_root():
 def about():
     return {"data":"about page"}
 
-@app.post("/blog")
-def add_blog(blog : Blog, db: Session= Depends()):
-    return db
-    return {"data" : {"title": blog.title, "description": blog.description, "published": blog.published},"message" : "New Blog Created"}
-
 
 @app.get("/blogs")
 def blog(limit:Optional[int] = 10, published:Optional[bool] = True, sort: Optional[str] = None):
@@ -36,5 +38,22 @@ def blog(limit:Optional[int] = 10, published:Optional[bool] = True, sort: Option
 @app.get("/blog/{id}")
 def blog(id):
     return {"data":{"blog_id": id}}
+
+
+# connection to database
+
+@app.post("/blog")
+def add_blog(blog : Blog, db: Session= Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, description=blog.description, published=blog.published)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+   
+
+@app.get("/blog")
+def get_blog(db: Session= Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
 
